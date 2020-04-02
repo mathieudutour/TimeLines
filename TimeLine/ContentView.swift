58d5e32a7312939f64f6e9aa93e8f1dcda95a4dd
Mathieu Dutour <mathieu@dutour.me>
2020-04-02 14:40:00 +0200

    Initial Commit

---
+++ TimeLine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimeLine
+//
+//  Created by Mathieu Dutour on 02/04/2020.
+//  Copyright © 2020 Mathieu Dutour. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
