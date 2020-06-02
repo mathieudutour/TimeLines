221ee299c9f04ec6d8d3fe89a57ce39cad6df3ed
mathieudutour <mathieu@dutour.me>
2020-06-02 16:06:54 +0200

    fix line in extreme latitudes

--- TimeLineShared/TimeLineShared/Line.swift
+++ TimeLineShared/TimeLineShared/Line.swift
@@ -30,65 +30,101 @@ fileprivate func pointFraction(_ date: Date?, _ timezone: TimeZone?) -> CGFloat?
 fileprivate let defaultLineWidth: CGFloat = 2
 fileprivate let defaultMaxHeight: CGFloat = 25
 
-fileprivate func pointInFrame(frame: CGRect, point: CGFloat, startPoint: CGFloat?, endPoint: CGFloat?) -> CGPoint {
+fileprivate func pointInFrame(frame: CGRect, point: CGFloat, timezone: TimeZone?, startTime: Date?, endTime: Date?, preSunset: Date?, postSunrise: Date?) -> CGPoint {
   guard
-    let startPoint = startPoint,
-    let endPoint = endPoint
+    var startPoint = pointFraction(startTime, timezone),
+    var endPoint = pointFraction(endTime, timezone)
   else {
+    // complete night time so always at the bottom
     return CGPoint(
       x: frame.origin.x + frame.width * CGFloat(point),
       y: frame.origin.y + frame.height
     )
   }
 
+  if !cal.isDateInToday(startTime!) {
+    startPoint -= 1
+  }
+  if !cal.isDateInToday(endTime!) {
+    endPoint += 1
+  }
+
+  if startPoint < 0 && endPoint > 1 {
+    // complete day time
+    return CGPoint(
+      x: frame.origin.x + frame.width * CGFloat(point),
+      y: frame.origin.y
+    )
+  }
+
   var y: CGFloat = frame.origin.y + frame.height
 
-  if point < endPoint && point > startPoint {
-    // find the position on the parabola
-    let w = frame.width * (endPoint - startPoint)
-    let x1 = frame.width * CGFloat(startPoint)
+  if let preSunsetPoint = pointFraction(preSunset, timezone), point < preSunsetPoint {
+    // find the position on the pre parabola
+    let w = 2 * frame.width * preSunsetPoint
+    let x1 = frame.width * -preSunsetPoint
     let c = y
     let b = -4 * frame.height / w
     let a = -b / w
-    let x = frame.width * CGFloat(point) - x1
+    let x = frame.width * point - x1
+    y = a * x * x + b * x + c
+  } else if let postSunrisePoint = pointFraction(postSunrise, timezone), point > postSunrisePoint {
+    // find the position on the post parabola
+    let w = 2 * frame.width * (1 - postSunrisePoint)
+    let x1 = frame.width * postSunrisePoint
+    let c = y
+    let b = -4 * frame.height / w
+    let a = -b / w
+    let x = frame.width * point - x1
+    y = a * x * x + b * x + c
+  } else if point < endPoint && point > startPoint {
+    // find the position on the parabola
+    let w = endPoint > 1 ? 2 * frame.width * (1 - startPoint)
+      : startPoint < 0 ? 2 * frame.width * endPoint
+      : frame.width * (endPoint - startPoint)
+    let x1 = startPoint < 0 ? 0 : frame.width * startPoint
+    let c = y
+    let b = -4 * frame.height / w
+    let a = -b / w
+    let x = frame.width * point - x1
     y = a * x * x + b * x + c
   }
 
   return CGPoint(
-    x: frame.origin.x + frame.width * CGFloat(point),
+    x: frame.origin.x + frame.width * point,
     y: y
   )
 }
 
-fileprivate func circlePosition(frame: CGRect, time: Date, timezone: TimeZone?, startTime: Date?, endTime: Date?) -> CGPoint {
+fileprivate func circlePosition(frame: CGRect, time: Date, timezone: TimeZone?, startTime: Date?, endTime: Date?, preSunset: Date?, postSunrise: Date?) -> CGPoint {
   guard let timePoint = pointFraction(time, timezone) else {
     // that can't happen
     return CGPoint(x: 0, y: 0)
   }
 
-  let startPoint = pointFraction(startTime, timezone)
-  let endPoint = pointFraction(endTime, timezone)
-
-  return pointInFrame(frame: frame, point: timePoint, startPoint: startPoint, endPoint: endPoint)
+  return pointInFrame(frame: frame, point: timePoint, timezone: timezone, startTime: startTime, endTime: endTime, preSunset: preSunset, postSunrise: postSunrise)
 }
 
 struct ParabolaLine: Shape {
   var timezone: TimeZone?
   var startTime: Date?
   var endTime: Date?
+  var preSunset: Date?
+  var postSunrise: Date?
 
   func path(in frame: CGRect) -> Path {
     var path = Path()
 
-    path.move(to: CGPoint(
-      x: frame.origin.x,
-      y: frame.origin.y + frame.height)
-    )
-
     guard
-      let startPoint = pointFraction(startTime, timezone),
-      let endPoint = pointFraction(endTime, timezone)
+      var startPoint = pointFraction(startTime, timezone),
+      var endPoint = pointFraction(endTime, timezone)
     else {
+      // complete night time
+      path.move(to: CGPoint(
+        x: frame.origin.x,
+        y: frame.origin.y + frame.height)
+      )
+
       path.addLine(to: CGPoint(
         x: frame.origin.x + frame.width,
         y: frame.origin.y + frame.height)
@@ -97,45 +133,158 @@ struct ParabolaLine: Shape {
       return path
     }
 
-    path.addLine(to: CGPoint(
-      x: frame.origin.x + frame.width * CGFloat(startPoint),
-      y: frame.origin.y + frame.height
-    ))
+    if !cal.isDateInToday(startTime!) {
+      startPoint -= 1
+    }
+    if !cal.isDateInToday(endTime!) {
+      endPoint += 1
+    }
+
+    if startPoint < 0 && endPoint > 1 {
+      // complete day time
+      path.move(to: CGPoint(
+        x: frame.origin.x,
+        y: frame.origin.y)
+      )
+
+      path.addLine(to: CGPoint(
+        x: frame.origin.x + frame.width,
+        y: frame.origin.y)
+      )
+
+      return path
+    }
+
+    if let preSunsetPoint = pointFraction(preSunset, timezone) {
+      // we have a sunset from the previous day
+      path.move(to: CGPoint(
+        x: frame.origin.x,
+        y: frame.origin.y)
+      )
+
+      path.addCurve(
+        to: CGPoint(
+          x: frame.origin.x + frame.width * preSunsetPoint,
+          y: frame.origin.y + frame.height
+        ),
+        control1: CGPoint(
+          x: frame.origin.x + frame.width * (preSunsetPoint * 4 / 5),
+          y: frame.origin.y
+        ),
+        control2: CGPoint(
+          x: frame.origin.x + frame.width * preSunsetPoint,
+          y: frame.origin.y + frame.height
+        )
+      )
+    } else if startPoint < 0 {
+      path.move(to: CGPoint(
+        x: frame.origin.x,
+        y: frame.origin.y)
+      )
+    } else {
+      path.move(to: CGPoint(
+        x: frame.origin.x,
+        y: frame.origin.y + frame.height)
+      )
+    }
+
+    if startPoint < 0 {
+      path.addCurve(
+        to: CGPoint(
+          x: frame.origin.x + frame.width * endPoint,
+          y: frame.origin.y + frame.height
+        ),
+        control1: CGPoint(
+          x: frame.origin.x + frame.width * (endPoint * 4 / 5),
+          y: frame.origin.y
+        ),
+        control2: CGPoint(
+          x: frame.origin.x + frame.width * endPoint,
+          y: frame.origin.y + frame.height
+        )
+      )
+    } else {
+      path.addLine(to: CGPoint(
+        x: frame.origin.x + frame.width * startPoint,
+        y: frame.origin.y + frame.height
+      ))
+    }
+
+    if endPoint > 1 {
+      path.addCurve(
+        to: CGPoint(
+          x: frame.origin.x + frame.width,
+          y: frame.origin.y
+        ),
+        control1: CGPoint(
+          x: frame.origin.x + frame.width * startPoint,
+          y: frame.origin.y + frame.height
+        ),
+        control2: CGPoint(
+          x: frame.origin.x + frame.width * (2 * (1 - startPoint) / 5 + startPoint),
+          y: frame.origin.y
+        )
+      )
+
+      return path
+    }
 
     path.addCurve(
       to: CGPoint(
-        x: frame.origin.x + frame.width * CGFloat((endPoint - startPoint) / 2 + startPoint),
+        x: frame.origin.x + frame.width * ((endPoint - startPoint) / 2 + startPoint),
         y: frame.origin.y
       ),
       control1: CGPoint(
-        x: frame.origin.x + frame.width * CGFloat(startPoint),
+        x: frame.origin.x + frame.width * startPoint,
         y: frame.origin.y + frame.height
       ),
       control2: CGPoint(
-        x: frame.origin.x + frame.width * CGFloat((endPoint - startPoint) / 5 + startPoint),
+        x: frame.origin.x + frame.width * ((endPoint - startPoint) / 5 + startPoint),
         y: frame.origin.y
       )
     )
 
     path.addCurve(
       to: CGPoint(
-        x: frame.origin.x + frame.width * CGFloat(endPoint),
+        x: frame.origin.x + frame.width * endPoint,
         y: frame.origin.y + frame.height
       ),
       control1: CGPoint(
-        x: frame.origin.x + frame.width * CGFloat((endPoint - startPoint) * 4 / 5 + startPoint),
+        x: frame.origin.x + frame.width * ((endPoint - startPoint) * 4 / 5 + startPoint),
         y: frame.origin.y
       ),
       control2: CGPoint(
-        x: frame.origin.x + frame.width * CGFloat(endPoint),
+        x: frame.origin.x + frame.width * endPoint,
         y: frame.origin.y + frame.height
       )
     )
 
-    path.addLine(to: CGPoint(
-      x: frame.origin.x + frame.width,
-      y: frame.origin.y + frame.height)
-    )
+    if let postSunrisePoint = pointFraction(postSunrise, timezone) {
+      path.addLine(to: CGPoint(
+        x: frame.origin.x + frame.width * postSunrisePoint,
+        y: frame.origin.y + frame.height
+      ))
+
+      path.addCurve(
+        to: CGPoint(
+          x: frame.origin.x + frame.width,
+          y: frame.origin.y
+        ),
+        control1: CGPoint(
+          x: frame.origin.x + frame.width * postSunrisePoint,
+          y: frame.origin.y + frame.height
+        ),
+        control2: CGPoint(
+          x: frame.origin.x + frame.width * (2 * (1 - postSunrisePoint) / 5 + postSunrisePoint),
+          y: frame.origin.y
+        )
+      )
+    } else {
+      path.addLine(to: CGPoint(
+        x: frame.origin.x + frame.width,
+        y: frame.origin.y + frame.height
+      ))
+    }
 
     return path
   }
@@ -146,13 +295,15 @@ struct CurrentTimeCircle: Shape {
   var timezone: TimeZone?
   var startTime: Date?
   var endTime: Date?
+  var preSunset: Date?
+  var postSunrise: Date?
 
   var lineWidth: CGFloat?
 
   func path(in frame: CGRect) -> Path {
     var path = Path()
 
-    let pos = circlePosition(frame: frame, time: now, timezone: timezone, startTime: startTime, endTime: endTime)
+    let pos = circlePosition(frame: frame, time: now, timezone: timezone, startTime: startTime, endTime: endTime, preSunset: preSunset, postSunrise: postSunrise)
 
     path.addArc(center: pos, radius: (lineWidth ?? defaultLineWidth) * 1.5, startAngle: Angle.zero, endAngle: Angle.degrees(360), clockwise: true)
 
@@ -165,6 +316,8 @@ struct CurrentTimeText: View {
   var timezone: TimeZone?
   var startTime: Date?
   var endTime: Date?
+  var preSunset: Date?
+  var postSunrise: Date?
 
   private let font = CPFont.systemFont(ofSize: 18)
 
@@ -179,7 +332,7 @@ struct CurrentTimeText: View {
       return .zero
     }
 
-    let pos = circlePosition(frame: frame, time: now, timezone: timezone, startTime: startTime, endTime: endTime)
+    let pos = circlePosition(frame: frame, time: now, timezone: timezone, startTime: startTime, endTime: endTime, preSunset: preSunset, postSunrise: postSunrise)
     let stringSize = getSize(string)
 
     var x = pos.x
@@ -201,8 +354,11 @@ struct CurrentTimeText: View {
         y = pointInFrame(
           frame: frame,
           point: rightCorner / frame.width,
-          startPoint: pointFraction(startTime, timezone),
-          endPoint: pointFraction(endTime, timezone)
+          timezone: timezone,
+          startTime: startTime,
+          endTime: endTime,
+          preSunset: preSunset,
+          postSunrise: postSunrise
         ).y - 20
       }
       x = x - stringSize.exact
@@ -212,8 +368,11 @@ struct CurrentTimeText: View {
         y = pointInFrame(
           frame: frame,
           point: leftCorner / frame.width,
-          startPoint: pointFraction(startTime, timezone),
-          endPoint: pointFraction(endTime, timezone)
+          timezone: timezone,
+          startTime: startTime,
+          endTime: endTime,
+          preSunset: preSunset,
+          postSunrise: postSunrise
         ).y - 20
       }
     }
@@ -281,19 +440,55 @@ public struct Line: View {
   }
 
   public var body: some View {
-    let solar = coordinate != nil ? Solar(coordinate: coordinate!) : nil
+    let solar = startTime == nil && endTime == nil && coordinate != nil ? Solar(coordinate: coordinate!) : nil
 
-    let start = startTime?.staticTime(timezone) ?? solar?.civilSunrise
-    let end = endTime?.staticTime(timezone) ?? solar?.civilSunset
+    var start = startTime?.staticTime(timezone) ?? solar?.civilSunrise
+    var end = endTime?.staticTime(timezone) ?? solar?.civilSunset
+
+    var postSunrise: Date? = nil
+    var preSunset: Date? = nil
+
+    if start != nil && !cal.isDateInToday(start!) {
+      // that means the sunrise was yesterday, and that there will be another one
+      // sometimes tonight
+      // so we try to get the sunrise of tomorrow which should be the one of tonight
+      let tomorrow = Date().addingTimeInterval(24 * 3600)
+      let tomorrowSolar = coordinate != nil ? Solar(for: tomorrow, coordinate: coordinate!) : nil
+      postSunrise = tomorrowSolar?.civilSunrise
+    }
+
+    if end != nil && !cal.isDateInToday(end!) {
+      // that means the sunset is tomorrow, and that there was be another one
+      // sometimes this morning
+      // so we try to get the sunset of yesterday which should be the one of this morning
+      let yesterday = Date().addingTimeInterval(-24 * 3600)
+      let yesterdaySolar = coordinate != nil ? Solar(for: yesterday, coordinate: coordinate!) : nil
+      preSunset = yesterdaySolar?.civilSunset
+    }
+
+    if start == nil, end == nil {
+      let month = cal.component(.month, from: Date())
+      let isSummer = month >= 4 && month < 10
+
+      if coordinate?.latitude ?? 0 > 0 && isSummer {
+        // if we are in summer in the northern emisphere
+        start = Date().addingTimeInterval(-48 * 3600)
+        end = Date().addingTimeInterval(48 * 3600)
+      } else if coordinate?.latitude ?? 0 < 0 && !isSummer {
+        // if we are in summer in the southern emisphere
+        start = Date().addingTimeInterval(-48 * 3600)
+        end = Date().addingTimeInterval(48 * 3600)
+      }
+    }
 
     let diff = Double(timezone?.diffInSecond() ?? 0) / (24 * 3600)
 
     return LineGeometryReader { p in
       ZStack(alignment: .topLeading) {
-        ParabolaLine(timezone: self.timezone, startTime: start, endTime: end)
-          .stroke(style: StrokeStyle(lineWidth: defaultLineWidth, lineCap: .round, lineJoin: .round))
-        CurrentTimeCircle(now: self.currentTime.now, timezone: self.timezone, startTime: start, endTime: end)
-        CurrentTimeText(now: self.currentTime.now, timezone: self.timezone, startTime: start, endTime: end)
+        ParabolaLine(timezone: self.timezone, startTime: start, endTime: end, preSunset: preSunset, postSunrise: postSunrise)
+          .stroke(style: StrokeStyle(lineWidth: self.lineWidth ?? defaultLineWidth, lineCap: .round, lineJoin: .round))
+        CurrentTimeCircle(now: self.currentTime.now, timezone: self.timezone, startTime: start, endTime: end, preSunset: preSunset, postSunrise: postSunrise)
+        CurrentTimeText(now: self.currentTime.now, timezone: self.timezone, startTime: start, endTime: end, preSunset: preSunset, postSunrise: postSunrise)
       }
       .frame(width: p.width, height: p.height)
       .gesture(
